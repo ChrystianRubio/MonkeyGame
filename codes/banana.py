03f1e35d07bdf6e4ac13eccd8307d8fdf6871dc5
Chrystian Rafael Rubio de Melo <49858310+ChrystianRubio@users.noreply.github.com>
2021-05-01 21:36:51 -0400

    Add files via upload

---
+++ codes/banana.py
@@ -0,0 +1,25 @@
+import pygame
+import random
+
+
+class Banana:
+    def __init__(self):
+        
+        self.sprites = []
+
+        self.x = 0
+        self.y = 0
+
+
+    def fall(self):
+        self.y += 1
+
+
+    def resetFall(self):
+        if self.y >= 410:
+            self.x = random.randint(0, 990)
+            self.y = random.randint(-400, 0)
+            self.y = -55
+
+
+
